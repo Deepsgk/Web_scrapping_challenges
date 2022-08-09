38236b3b072467a8aa746e2da6fad152d43c54fb
Deepa Nair <104196538+Deepsgk@users.noreply.github.com>
2022-08-10 02:56:01 +1000

    app.py change

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 @app.route("/")
-def index():
+def echo():
     final_mars_data = mongo.db.final_mars_data.find_one()
     return render_template("index.html", final_mars_data=final_mars_data)
 
 @app.route("/scrape")
 def scrape():
-    final_mars_data = mongo.db.final_mars_data
+    
     final_mars_data = scrape_mars.scrape_all()
-    final_mars_data.update({},{"$set":final_mars_data}, upsert=True)
+    mongo.db.final_mars_data.update_one({},{"$set":final_mars_data}, upsert=True)
     return redirect('/', code=302)
     
 if __name__ == "__main__":
